1b900a202007ee30777061d367b4477ccbf64f75
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2021-07-22 11:22:42 -0400

    Initial commit

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+export const LandingPage = () => (
+  <Button variant="contained" color="primary">
+      Hello World
+  </Button>
+);

1f3c6e138a24acb793564421c9d64e0c1ae90ae2
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2021-07-22 20:05:26 -0400

    Implemented API for getting unique characters counts from email addresses.

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -24,4 +24,41 @@ class  Api::PeopleController < Api::BaseController
 
         render json: people
     end
+
+    def email_character_counts
+        response = @conn.get
+        response_body = JSON.parse(response.body, :symbolize_names => true)
+        people = response_body[:data]
+        email_addresses = people.map do |hash|
+            hash[:email_address]
+        end
+
+        character_counts = count_unique_characters_in email_addresses
+        email_character_tallies = tally_up character_counts
+        render json: email_character_tallies
+    end
+
+    private
+
+    def count_unique_characters_in(words)
+        character_counts = {}
+
+        words.each do |word|
+            word.each_char do |char|
+                if character_counts.include?(char)
+                    character_counts[char] += 1
+                else
+                    character_counts[char] = 1
+                end
+            end
+        end
+
+        character_counts
+    end
+
+    def tally_up(counts)
+        counts.map do |k, v|
+            {:character => k, :count => v}
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   namespace :api, defaults: { format: "json" } do
-    get :me, to: 'me#me'
     get :people, to: 'people#index'
+    get :email_character_counts, to: 'people#email_character_counts'
   end
 
   root to: "main#index"

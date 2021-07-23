9be4b776212e244cc9c5bf63bca6897be8666738
Marivaldi <55033038+Marivaldi@users.noreply.github.com>
2021-07-23 10:51:03 -0400

    Implemented Route: GET /api/possible_duplicate_email_addresses

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     get :people, to: 'people#index'
     get :email_character_counts, to: 'people#email_character_counts'
+    get :possible_duplicate_email_addresses, to: 'people#possible_duplicate_email_addresses'
   end
 
   root to: "main#index"
